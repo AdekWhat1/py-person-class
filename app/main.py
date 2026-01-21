84c02a273245325f9c62fc0d29f53ce76367af89
Den <mr.denisyakubenko@gmail.com>
2026-01-21 16:01:34 +0200

    Solution3

--- app/main.py
+++ app/main.py
@@ -4,8 +4,10 @@ class Person:
     def __init__(self, name: str, age: int) -> None:
         self.name = name
         self.age = age
+        Person.people[self.name] = self
 
 def create_person_list(people: list) -> list:
+    Person.people.clear()
     people_objs = [Person(person_data["name"], person_data["age"]) for person_data in people]
     for person_data in people:
         if person_data.get("wife") is not None:
